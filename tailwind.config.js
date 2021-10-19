f14b1d2dd21b105141e3898b3181338dc4ad54c2
Andry Pebrianto <andrypeb227@gmail.com>
2021-10-19 23:45:03 +0700

    Belajar membuat custom breakpoint untuk screen max-width dan menerapkannya pada navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,26 @@
 module.exports = {
-  purge: [
-    './src/**/*.html',
-    './src/**/*.js',
-  ],
+  purge: ["./src/**/*.html", "./src/**/*.js"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        "mw-2xl": { max: "1535px" },
+        // => @media (max-width: 1535px) { ... }
+
+        "mw-xl": { max: "1279px" },
+        // => @media (max-width: 1279px) { ... }
+
+        "mw-lg": { max: "1023px" },
+        // => @media (max-width: 1023px) { ... }
+
+        "mw-md": { max: "767px" },
+        // => @media (max-width: 767px) { ... }
+
+        "mw-sm": { max: "639px" },
+        // => @media (max-width: 639px) { ... }
+      },
+    },
   },
   variants: {},
   plugins: [],
-}
+};
